b19ea66904ec3e60b113cfc472a5d5f59f635674
gautam <gautam@propertyguru.com.sg>
2018-11-11 12:36:59 +0000

    fixed distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='feature_utils',
-      version='0.1',
+      version='0.1a',
       description='Utilities for generating features for machine learning pipelines',
       author='Gautam Borgohain',
       author_email='gautamborgohain90@gmail.com',
       license='MIT',
-      packages=['feature_utils'],
+      packages=['feature_utils','feature_utils.imports', 'feature_utils.utils'],
       install_requires=['sklearn','numpy','pandas', 'tqdm','lightgbm'],
       zip_safe=False)
 
